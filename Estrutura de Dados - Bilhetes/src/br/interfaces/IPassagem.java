7ae7aa6e15554bda188c47ea9ef786d4c8180405
EliasJunior2610 <e26junior2002@gmail.com>
2023-05-15 13:07:39 -0300

    Update

--- Estrutura de Dados - Bilhetes/src/br/interfaces/IPassagem.java
+++ Estrutura de Dados - Bilhetes/src/br/interfaces/IPassagem.java
@@ -1,14 +1,14 @@
 public interface IPassagem {
-    //abre uma classe boolean, já que, é possivel escolher apenas dois valores, se vai ou não ir pro camarote.
-    public boolean camarote = true;
+    //abre uma classe boolean, já que, é possivel escolher apenas dois valores, se vai ou não ir pra primeira classe.
+    public boolean primeira_classe = true;
     // método utilizado no Serviço.
-    // enumeração para os valores possíveis do gênero dos filmes
-    enum Genero {
-        FANTASIA,
-        ANIVERSARIO,
-        CASAMENTO,
-        FORMATURA,
-        TERROR
+    // enumeração para os valores possíveis do destino da viagem;
+    enum Estado {
+        PERNAMBUCO,
+        PARAIBA,
+        ALAGOAS,
+        SERGIPE,
+        AMAZONAS
     }
     public void cadastrar(double preço, double horário, int quantidade);
 }
